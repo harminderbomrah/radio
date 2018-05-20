185530af85bd2ce7d401f2bda03862dd253a2ea4
Bomrah Harry <harry@Bomrahde-MacBook-Pro.local>
2018-05-20 13:18:27 +0800

    initial update, can only work after real data

---
+++ js/config.js
@@ -0,0 +1,30 @@
+var app = {};
+
+app.node_types = [
+	{slug: 'bt_tactic',       name: 'Tactic', 			names: 'Tactics', 		 color: '#985D94' },
+	{slug: 'bt_principle',    name: 'Principle', 		names: 'Principles', 	 color: '#625784' },
+	{slug: 'bt_theory',       name: 'Theory', 			names: 'Theories', 		 color: '#308DAF' },
+	{slug: 'bt_case',         name: 'Case Study', 		names: 'Case Studies',   color: '#045E79' },
+	{slug: 'bt_practitioner', name: 'Practitioner', 	names: 'Practitioners',  color: '#1E1A39' },
+	{slug: 'bt_tactic',       name: 'Tactic', 			names: 'Tactics', 		 color: '#891D48' },
+	{slug: 'bt_principle',    name: 'Principle', 		names: 'Principles', 	 color: '#B8687F' },
+	{slug: 'bt_theory',       name: 'Theory', 			names: 'Theories', 		 color: '#D4839F' },
+	{slug: 'bt_case',         name: 'Case Study', 		names: 'Case Studies',   color: '#EE7D5F' },
+	{slug: 'bt_practitioner', name: 'Practitioner', 	names: 'Practitioners',  color: '#E84356' },
+	{slug: 'bt_tactic',       name: 'Tactic', 			names: 'Tactics', 		 color: '#F2B292' },
+	{slug: 'bt_principle',    name: 'Principle', 		names: 'Principles', 	 color: '#F0A42F' },
+	{slug: 'bt_theory',       name: 'Theory', 			names: 'Theories', 		 color: '#DECC81' },
+	{slug: 'bt_case',         name: 'Case Study', 		names: 'Case Studies',   color: '#FAF7C0' },
+	{slug: 'bt_practitioner', name: 'Practitioner', 	names: 'Practitioners',  color: '#DBDBDB' },
+	{slug: 'bt_tactic',       name: 'Tactic', 			names: 'Tactics', 		 color: '#83B2DF' },
+	{slug: 'bt_principle',    name: 'Principle', 		names: 'Principles', 	 color: '#40B7BE' },
+	{slug: 'bt_theory',       name: 'Theory', 			names: 'Theories', 		 color: '#0E7D7D' },
+	{slug: 'bt_case',         name: 'Case Study', 		names: 'Case Studies',   color: '#7BAC9B' },
+	{slug: 'bt_practitioner', name: 'Practitioner', 	names: 'Practitioners',  color: '#4FAF7E' }
+];
+
+app.dataFile 	= 'relation.json';
+app.showType 	= false;
+app.title 		= "Radio";
+app.url 		= "http://beautifultrouble.org/";
+
